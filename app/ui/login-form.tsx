0cf015e2bb233ce43f280b23cb2c6c48a2c390c1
Vldadimir Ilin <61191801+linkinvo95@users.noreply.github.com>
2024-08-29 19:47:15 +0300

    cvbnmj

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -7,16 +7,18 @@ import {
 } from '@heroicons/react/24/outline'
 import { ArrowRightIcon } from '@heroicons/react/20/solid'
 import { Button } from './button'
-import { useActionState } from 'react'
-import { authenticate } from '@/app/lib/actions'
+// import { useActionState } from 'react'
+// import { authenticate } from '@/app/lib/actions'
 
 export default function LoginForm() {
-	const [errorMessage, formAction, isPending] = useActionState(
-		authenticate,
-		undefined
-	)
+	// const [errorMessage, formAction, isPending] = useActionState(
+	// 	authenticate,
+	// 	undefined
+	// )
 	return (
-		<form action={formAction} className='space-y-3'>
+		<form 
+		// action={formAction} 
+		className='space-y-3'>
 			<div className='flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8'>
 				<h1 className={`${lusitana.className} mb-3 text-2xl`}>
 					Please log in to continue.
@@ -62,7 +64,9 @@ export default function LoginForm() {
 						</div>
 					</div>
 				</div>
-				<Button className='mt-4 w-full' aria-desabled={isPending}>
+				<Button className='mt-4 w-full' 
+				// aria-desabled={isPending}
+				>
 					Log in <ArrowRightIcon className='ml-auto h-5 w-5 text-gray-50' />
 				</Button>
 				<div
@@ -70,12 +74,12 @@ export default function LoginForm() {
 					aria-live='polite'
 					aria-atomic='true'
 				>
-					{errorMessage && (
+					{/* {errorMessage && (
 						<>
 							<ExclamationCircleIcon className='h-5 w-5 text-red-500' />
 							<p className='text-sm text-red-500'>{errorMessage}</p>
 						</>
-					)}
+					)} */}
 				</div>
 			</div>
 		</form>
